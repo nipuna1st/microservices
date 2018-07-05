6a029484ee033aceef683b27ab5226c871cfdf39
Nipuna Madushan <Nipuna_lspl@dialog.dialoggsm.com>
2018-07-05 18:35:01 +0530

    Services commited

---
+++ com-dashboard-service/src/main/java/com/dashboardservice/DashboardServiceApplication.java
@@ -0,0 +1,24 @@
+package com.dashboardservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@EnableDiscoveryClient
+@EnableFeignClients
+@SpringBootApplication
+public class DashboardServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DashboardServiceApplication.class, args);
+	}
+	
+	@Bean
+	public RestTemplate restTemplate(RestTemplateBuilder builder) {
+		return builder.build();
+	}
+}
---
+++ com-employee-service/src/main/java/com/employeeservice/controller/EmployeeSearchController.java
@@ -0,0 +1,29 @@
+package com.employeeservice.controller;
+
+import java.util.Collection;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.employeeservice.domain.Employee;
+import com.employeeservice.service.EmployeeSearchService;
+
+@RefreshScope
+@RestController
+public class EmployeeSearchController {
+	@Autowired
+	EmployeeSearchService employeeSearchService;
+
+	@RequestMapping("/employee/find/{id}")
+	public Employee findById(@PathVariable Long id) {
+		return employeeSearchService.findById(id);
+	}
+
+	@RequestMapping("/employee/findall")
+	public Collection<Employee> findAll() {
+		return employeeSearchService.findAll();
+	}
+}
